4845f34535eac82245f7215dc2c6af75fd25bbf8
Riddhika Desmukh <144925058+dsmkh07@users.noreply.github.com>
2025-07-15 10:16:48 +0530

    Delete Loadbalancer-CPP-main directory

--- Loadbalancer-CPP-main/load-balancer/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "LoadBalancer.h"
-#include <vector>
-
-int main() {
-    std::vector<int> backend_ports = {8001, 8002, 8003};
-    LoadBalancer lb(8080, backend_ports);
-    lb.start();
-    return 0;
-}
